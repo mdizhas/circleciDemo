92d1a23c45f88cd057fdadbfb966ca3ff3b4a9d2
izhasgit <mohammed.izhas@espire.com>
2022-06-27 14:24:33 +0530

    commit changes

--- specs/emt/e2e.login.js
+++
@@ -1,9 +0,0 @@
-const { faker } = require('@faker-js/faker')
-const LoginPage = require('../../pageobjects/common/login.page')
-const LogoutPage = require('../../pageobjects/common/logout.page')
-const LoginCredentialsData = require('../../pageobjects/testData/logincredentials.data')
-const AllynetCommon = require('../../pageobjects/common/allynetcommon')
-
-describe('EMT Login / Logout', async () => {
-await AllynetCommon.processLogin(process.env.EMT_USERNAME,process.env.EMT_PASSWORD,'invalidEMTpassword')
-})
