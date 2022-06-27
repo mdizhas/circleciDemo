d847a78ac7acab7c7681120db33503fec9a0c267
harsha shirali <107995304+harsha-espire@users.noreply.github.com>
2022-06-27 20:33:25 +0530

    changes recent to circle/ci

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,29 +1,18 @@
-exports.config = {
-  specs: [
-    './specs/*/**.js'
-  ],
-  exclude: [
-  ],
-  maxInstances: 10,
-  capabilities: [{
-      maxInstances: 5,
-      browserName: 'chrome',
-      'goog:chromeOptions': {
-        'args': ["--use-fake-device-for-media-stream", "--use-fake-ui-for-media-stream"]
-    },
-      acceptInsecureCerts: true
-  }],
-  logLevel: 'info',
-  bail: 0,
-  baseUrl: 'http://localhost',
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 120000,
-  connectionRetryCount: 3,
-  services: ['chromedriver'],
-  framework: 'mocha',
-  reporters: ['spec'],
-  mochaOpts: {
-      ui: 'bdd',
-      timeout: 250000
-  },
+// username = process.env.BROWSERSTACK_USERNAME
+accessKey = process.env.BROWSERSTACK_ACCESS_KEY
+browserstackLocal = process.env.BROWSERSTACK_LOCAL
+browserstackLocalIdentifier = process.env.BROWSERSTACK_LOCAL_IDENTIFIER
+
+var capabilities = {
+ "os" : "Windows",
+ "browser" : "chrome",
+ "browserstack.local" : browserstackLocal,
+ "browserstack.localIdentifier" : browserstackLocalIdentifier,
+ "browserstack.user" : username,
+ "browserstack.key" : accessKey
 }
+
+var driver = new webdriver.Builder().
+  usingServer("https://hub-cloud.browserstack.com/wd/hub").
+  withCapabilities(capabilities).
+  build();
