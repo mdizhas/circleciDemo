b1daa6faef434f50261bd43c2a4c3557ad96e75b
izhasgit <mohammed.izhas@espire.com>
2022-06-27 16:15:51 +0530

    commit changes

--- pageobjects/testData/profiledata.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    first_name: 'Automation',
-    last_name: 'Test',
-    email: 'automation+md@mdally.com',
-    new_password: 'india@12345',
-    confirm_password: 'india@12345',
-    current_password: 'india@12345'
-}
